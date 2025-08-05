df9276dd7eb2efb267623c21a65a79e06e382368
deltea <thcheetah777@gmail.com>
2025-08-04 18:35:53 -0700

    play youtube videos very well

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,23 +1,41 @@
 <script lang="ts">
-  import { onMount } from "svelte";
+  import { onDestroy, onMount } from "svelte";
   import YoutubePlayer from "youtube-player";
+  import type { YouTubePlayer } from "youtube-player/dist/types";
 
-  let player;
+  let player: YouTubePlayer;
+  let isPlayerLoaded = $state(false);
 
   onMount(async () => {
+  });
+
+  onDestroy(() => {
+    if (player) player.destroy();
+  });
+
+  async function turnOnTV() {
     player = YoutubePlayer("video-player", {
-      playerVars: { controls: 0 }
+      playerVars: { controls: 0, loop: 1 }
     });
     player.loadVideoById("85VQEzwzAvE");
+    // player.loadVideoById("MJbE3uWN9vE");
     await player.playVideo();
     console.log("video started");
-  })
+    isPlayerLoaded = true;
+    console.log(isPlayerLoaded);
+  }
 </script>
 
-<main class="flex justify-center items-center h-screen">
-  <!-- iframe will replace this div -->
-  <div
-    id="video-player"
-    class="border-4 border-muted rounded-2xl"
-  ></div>
+<main class="flex flex-col gap-16 justify-center items-center h-screen">
+  <div class={isPlayerLoaded ? "block" : "hidden"}>
+    <!-- iframe will replace this div -->
+    <div
+      id="video-player"
+      class="border-4 border-muted rounded-2xl pointer-events-none"
+    ></div>
+  </div>
+
+  {#if !isPlayerLoaded}
+    <button onclick={turnOnTV}>turn on the tv</button>
+  {/if}
 </main>
